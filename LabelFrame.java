e16741ae33a28a607c3f7b00bb350c25a7354e47
musharofahmed1 <musharofahmed11@gmail.com>
2023-11-17 23:24:41 -0600

    MouseTracker

---
+++ LabelFrame.java
@@ -0,0 +1,17 @@
+import java.awt.FlowLayout;  
+import javax.swing.JFrame;  
+import javax.swing.JLabel;  
+import javax.swing.SwingConstants;  
+import javax.swing.Icon;  
+import javax.swing.ImageIcon; 
+
+public class LabelFrame extends JFrame{
+    private final JLabel label1; 
+    private final JLabel label2;  
+    private final JLabel label3;
+    
+    public LabelFrame(){
+
+    }
+    
+}
--- TextFieldFrame.java
+++ TextFieldFrame.java
@@ -6,13 +6,13 @@ import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JOptionPane;
 
-public class TextFieldFrame extends JFrame{
+public class TextFieldFrame extends JFrame {
     private final JTextField textField1;
     private final JTextField textField2;
     private final JTextField textField3;
     private final JPasswordField passwordField;
 
-    public TextFieldFrame(){
+    public TextFieldFrame() {
         super("Testing JTextField & JPasswordField");
         setLayout(new FlowLayout());
 
@@ -27,32 +27,38 @@ public class TextFieldFrame extends JFrame{
         add(textField3);
 
         passwordField = new JPasswordField("Hidden text");
-        add(passwordField); 
+        add(passwordField);
 
         TextFieldHandler handler = new TextFieldHandler();
         textField1.addActionListener(handler);
         textField2.addActionListener(handler);
-        textField3.addActionListener(handler); 
-        passwordField.addActionListener(handler); 
+        textField3.addActionListener(handler);
+        passwordField.addActionListener(handler);
     }
 
-    private class TextFieldHandler implements ActionListener{
+    private class TextFieldHandler implements ActionListener {
         @Override
-        public void actionPerformed(ActionEvent event){
+        public void actionPerformed(ActionEvent event) {
             String string = "";
-            if (event.getSource() == textField1)
-            string = String.format("textField1: %s",event.getActionCommand());
 
-            else if (event.getSource() == textField2)
-            string = String.format("textField2: %s",event.getActionCommand());
-
-            else if (event.getSource() == textField3)
-            string = String.format("textField3: %s",event.getActionCommand());
-
-            else if (event.getSource() == passwordField)
-            string = String.format("passwordField: %s",event.getActionCommand());
+            if (event.getSource() == textField1) {
+                string = String.format("textField1: %s", event.getActionCommand());
+            } else if (event.getSource() == textField2) {
+                string = String.format("textField2: %s", event.getActionCommand());
+            } else if (event.getSource() == textField3) {
+                string = String.format("textField3: %s", event.getActionCommand());
+            } else if (event.getSource() == passwordField) {
+                string = String.format("passwordField: %s", event.getActionCommand());
+            }
 
             JOptionPane.showMessageDialog(null, string);
         }
+    }
+
+    public static void main(String[] args) {
+        TextFieldFrame textFieldFrame = new TextFieldFrame();
+        textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textFieldFrame.setSize(400, 150);
+        textFieldFrame.setVisible(true);
     }
 }
