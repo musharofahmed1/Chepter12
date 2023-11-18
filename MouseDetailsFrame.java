aa045d5490a2702332da519e6a6bf5100d03c168
musharofahmed1 <musharofahmed11@gmail.com>
2023-11-17 23:45:21 -0600

    MouseDetailsFrame

---
+++ MouseDetailsFrame.java
@@ -0,0 +1,44 @@
+import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MouseDetailsFrame extends JFrame {
+    private String details;
+    private final JLabel statusBar;
+
+    public MouseDetailsFrame() {
+        super("Mouse clicks and buttons");
+
+        statusBar = new JLabel("Click the mouse");
+        add(statusBar, BorderLayout.SOUTH);
+        addMouseListener(new MouseClickHandler());
+    }
+
+    private class MouseClickHandler extends MouseAdapter {
+
+        @Override
+        public void mouseClicked(MouseEvent event) {
+            int xPos = event.getX();
+            int yPos = event.getY();
+
+            details = String.format("Clicked %d time(s)", event.getClickCount());
+
+            if (event.isMetaDown())
+                details += " with right mouse button";
+            else if (event.isAltDown())
+                details += " with center mouse button";
+            else
+                details += " with left mouse button";
+            statusBar.setText(details);
+        }
+    }
+
+    public static void main(String[] args) {
+        MouseDetailsFrame mouseDetailsFrame = new MouseDetailsFrame();
+        mouseDetailsFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mouseDetailsFrame.setSize(400, 150);
+        mouseDetailsFrame.setVisible(true);
+    }
+}
